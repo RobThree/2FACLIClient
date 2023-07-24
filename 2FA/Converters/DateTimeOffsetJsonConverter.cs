95d4dbb7eabee79f8a458352e0fd949c0ac74f7e
RobIII <r.janssen@keensystems.eu>
2023-07-24 17:42:52 +0200

    Autodetect milliseconds vs seconds in LP export (see #1 )

--- 2FA/Converters/DateTimeOffsetJsonConverter.cs
+++ 2FA/Converters/DateTimeOffsetJsonConverter.cs
@@ -5,8 +5,15 @@ namespace TwoFA.Converters;
 
 internal class DateTimeOffsetJsonConverter : JsonConverter<DateTimeOffset>
 {
+    private static readonly long _maxseconds = DateTimeOffset.MaxValue.ToUnixTimeSeconds();
     public override DateTimeOffset Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-        => DateTimeOffset.FromUnixTimeSeconds(reader.GetInt64());
+    {
+        var value = reader.GetInt64();
+        // Apparently there are timestamps in seconds and in milliseconds format (see https://github.com/RobThree/2FACLIClient/issues/1)
+        // This 'detects' which format is most likely in use and parses the value accordingly as milliseconds or seconds
+        return (value > _maxseconds) ? DateTimeOffset.FromUnixTimeMilliseconds(value) : DateTimeOffset.FromUnixTimeSeconds(value);
+    }
+
     public override void Write(Utf8JsonWriter writer, DateTimeOffset value, JsonSerializerOptions options)
-        => writer.WriteNumberValue(value.ToUnixTimeSeconds());
+        => writer.WriteNumberValue(value.ToUnixTimeMilliseconds());
 }
